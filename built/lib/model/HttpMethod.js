47edf77fe9e32764c9bcc29da61e2ba186bd47a4
Alexander Dorow <alexander.dorow@senior.com.br>
2020-03-11 09:40:31 -0300

    removed built

--- built/lib/model/HttpMethod.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var HttpMethod;
-(function (HttpMethod) {
-    HttpMethod["POST"] = "POST";
-    HttpMethod["GET"] = "GET";
-})(HttpMethod = exports.HttpMethod || (exports.HttpMethod = {}));
