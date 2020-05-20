146e5f370d91756eed866201f36a27d22a74a2fb
andre.rezende <andre.rezende@senior.com.br>
2020-05-20 13:20:28 -0300

    Added RequestClient to export class in index.ts.

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import SeniorApi from "./lib/SeniorApi";
 import FilterBuilder from "./lib/base/FilterBuilder";
+import RequestClient from "./lib/base/RequestClient";
 
-export { SeniorApi, FilterBuilder }
+export { SeniorApi, FilterBuilder, RequestClient }
