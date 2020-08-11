edae4215565f02300af49aeb6219874872a8e510
Wallace Reetz <wallace.reetz@gmail.com>
2020-08-11 13:47:22 -0300

    ajustes na Entity.ts

--- lib/base/Entity.ts
+++ lib/base/Entity.ts
@@ -41,9 +41,10 @@ export class Entity<T> extends RequestClient {
    * @param {T} entity um objeto da entidade a ser criada.
    * @param {string} id Id da entidade a ser substituida. Opcional.
    * @returns {Promise<RequestReturn>} retorna uma promise com a resposta da plataforma contendo no atributo `data` 
-   * o valor do id criado, atributo no padrão: `idNomeDaEntidade`. 
+   * o valor do id criado, atributo no padrão: `idNomeDaEntidade` ou um objeto com os valores da entidade, 
+   * depende do serviço a ser chamado.
    * 
-   * Ex: `{ idUsuario: 'uuid_gerado' }`
+   * Ex: `{ idUsuario: 'uuid_gerado' }` ou `{ id: 'uuid_gerado', name: 'nome do usuario' }`.
    */
   post(entity: T, id?: string): Promise<RequestReturn> {
     let url = `entities/${this.#entityName}`;
@@ -61,6 +62,18 @@ export class Entity<T> extends RequestClient {
     return this.request(clientOptions);
   }
 
+  /**
+   * Faz um put de um objeto da entidade passando um id para substituir a entidade. 
+   * 
+   * Observação: O id precisa estar dentro do objeto da entidade também.
+   * @param {string} id Id da entidade a ser substituida.
+   * @param {T} entity um objeto da entidade a ser criada.
+   * @returns {Promise<RequestReturn>} retorna uma promise com a resposta da plataforma contendo no atributo `data` 
+   * o valor do id criado, atributo no padrão: `idNomeDaEntidade` ou um objeto com os valores da entidade, 
+   * depende do serviço a ser chamado.
+   * 
+   * Ex: `{ idUsuario: 'uuid_gerado' }` ou `{ id: 'uuid_gerado', name: 'nome do usuario' }`.
+   */
   put(id: string, entity: T): Promise<RequestReturn> {
     const clientOptions = {
       url: this.getUrlPath(`entities/${this.#entityName}/${id}`),
@@ -73,6 +86,12 @@ export class Entity<T> extends RequestClient {
     return this.request(clientOptions);
   }
 
+  /**
+   * Faz um delete da entidade com base no id informado. 
+   * @param {string} id Id da entidade a ser deletada.
+   * @returns {Promise<RequestReturn>} retorna uma promise com a resposta da plataforma 
+   * podendo conter o id da entidade deletada ou um objeto vazio, depende do serviço chamado.
+   */
   delete(id: string): Promise<RequestReturn> {
     const clientOptions = {
       url: this.getUrlPath(`entities/${this.#entityName}/${id}`),
