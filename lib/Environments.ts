751c86ac449dd84d59312775bd02eb9819436079
Sérgio Souza <sergio.lcs@gmail.com>
2021-02-17 16:14:19 -0300

    Correção na URL de Produção

--- lib/Environments.ts
+++ lib/Environments.ts
@@ -1,4 +1,4 @@
 export enum ENVIRONMENTS {
-  DEV = 'https://platform-homologx.senior.com.br/t/senior.com.br/bridge/1.0/rest',
+  DEV = 'https://platform-homologx.senior.com.br/t/senior.com.br/bridge/1.0',
   PROD = 'https://api.senior.com.br',
 }
